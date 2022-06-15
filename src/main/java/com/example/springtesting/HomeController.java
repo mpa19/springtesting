5dead5a7950a1cfca877be0d4ad1982ce91d8e59
Marc Perez <marcperezarnaiz@gmail.com>
2022-06-15 18:03:58 +0200

    Añadidos nuevos test

--- src/main/java/com/example/springtesting/HomeController.java
+++ src/main/java/com/example/springtesting/HomeController.java
@@ -1,14 +1,21 @@
 package com.example.springtesting;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class HomeController {
 
     @RequestMapping(path="/greet")
     public @ResponseBody String greeting() {
         return "Hello, World";
+    }
+
+    @GetMapping("/add")
+    public Object add(
+        @RequestParam(value="a", defaultValue = "0") Float a,
+        @RequestParam(value="b", defaultValue = "0") Float b)
+    {
+        return a+b;
     }
 }
--- src/test/java/com/example/springtesting/HttpRequestTest.java
+++ src/test/java/com/example/springtesting/HttpRequestTest.java
@@ -1,14 +1,18 @@
 package com.example.springtesting;
 
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HttpRequestTest {
@@ -22,5 +26,24 @@ class HttpRequestTest {
     public void greetingShouldReturnDefaultMessage() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/greet",
                 String.class)).contains("Hello, World");
+    }
+
+    @Test
+    public void catADD() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/add?a=1&b=2",
+                String.class)).isEqualTo("3.0");
+    }
+
+    @Test
+    public void catADDWithIncorrectNumber() {
+
+        assertThat(this.restTemplate.getForEntity("http://localhost:" + port + "/add?a=1&b=x",
+                String.class).getStatusCode().is4xxClientError()).isTrue();
+    }
+
+    @Test
+    public void catADDWithoutNumber() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/add?a=1",
+                String.class)).isEqualTo("1.0");
     }
 }
