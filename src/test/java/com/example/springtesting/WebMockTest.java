54120b348f230996ce06860b883114e170a3f225
Marc Perez <marcperezarnaiz@gmail.com>
2022-06-15 12:51:22 +0200

    Tests with Service

--- src/test/java/com/example/springtesting/WebMockTest.java
+++ src/test/java/com/example/springtesting/WebMockTest.java
@@ -7,20 +7,26 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
-class WeLayerTest {
+@WebMvcTest(GreetingController.class)
+class WebMockTest {
 
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private GreetingService service;
+
     @Test
-    void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("Hello, World")));
+    void greetingShouldReturnMessageFromService() throws Exception {
+        when(service.greet()).thenReturn("Hello, Mock");
+
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("Hello, Mock")));
     }
 }
