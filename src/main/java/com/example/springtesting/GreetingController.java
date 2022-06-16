e49555c54358e3f35229d1f24d9526d196554ecf
Marc Perez <marcperezarnaiz@gmail.com>
2022-06-16 14:50:09 +0200

    Añadido pmd

--- src/main/java/com/example/springtesting/GreetingController.java
+++ src/main/java/com/example/springtesting/GreetingController.java
@@ -1,21 +1,16 @@
 package com.example.springtesting;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.PostConstruct;
-
 @Controller
 public class GreetingController {
 
-    private final GreetingService service;
+    private static GreetingService service;
 
     public GreetingController(GreetingService service) {
-        this.service = service;
+        GreetingController.service = service;
     }
 
     @RequestMapping(path="/")
--- src/main/java/com/example/springtesting/GreetingService.java
+++ src/main/java/com/example/springtesting/GreetingService.java
@@ -8,4 +8,6 @@ public class GreetingService {
     public String greet() {
         return "Hello, World";
     }
+
+
 }
